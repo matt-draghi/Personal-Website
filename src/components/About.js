cc893c97152047b2d533c56b703c347e1dc275e0
Matt Draghi <mtd5264@gmail.com>
2022-02-24 15:47:50 -0500

    Fade in added to About

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,23 @@
 import photo from "../assets/AE0483E6-43E0-4602-8653-74C064B79661.jpeg"
+import FadeIn from "react-fade-in/lib/FadeIn"
 
 function About(){
     return(
         <div className="about-container">
-            <h1>about.</h1>
-            {/* TODO: add 30 second pitch */}
-            <p>
-                Hi, my name is Matt Draghi, and I am a software engineering student at Flatiron School! 
-                Prior to my time at Flatiron I worked at Grant Thornton LLP for a year and a half as a member of their cyber risk team. 
-                Before entering the consulting space I graduated from Penn State University's College of Information Sciences and Technology, where I received a degree in cyber security, as well as in application design & development.
-                </p>
-            <img src={photo} alt="Rudy on the lion"/>
-            <p>
-                I am currently looking to utilize my knowledge of JavaScript and React to help innovate in the tech community. 
+            <FadeIn>
+                <h1>about.</h1>
+                {/* TODO: add 30 second pitch */}
+                <p>
+                    Hi, my name is Matt Draghi, and I am a software engineering student at Flatiron School! 
+                    Prior to my time at Flatiron I worked at Grant Thornton LLP for a year and a half as a member of their cyber risk team. 
+                    Before entering the consulting space I graduated from Penn State University's College of Information Sciences and Technology, where I received a degree in cyber security, as well as in application design & development.
+                    </p>
+                <img src={photo} alt="Rudy on the lion"/>
+                <p>
+                    I am currently looking to utilize my knowledge of JavaScript and React to help innovate in the tech community. 
 
-            </p>
+                </p>
+            </FadeIn>
         </div>
     )
 }
